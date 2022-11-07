e364acbe8228810f1fa39b554cdcd92d5ea7b61f
Héric Virgilio <102180379+HericVirgilio@users.noreply.github.com>
2022-11-06 23:41:59 -0300

    commit 6

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import UltimosLancamentos from './componentes/UltimosLancamentos';
 const AppContainer = styled.div`
     max-width: 100vw;
-    height: 170vh;
+    height: 140vh;
     background-image: linear-gradient(90deg,#002D52 35%,#326589);
 `
 
